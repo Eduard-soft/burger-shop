26843cacda6fc298e79423b22f60ea392810fa42
Eduard-soft <92379627+Eduard-soft@users.noreply.github.com>
2023-03-21 21:59:04 +0300

    Adding images and working with SEO in Head

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,22 @@
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const NotFoundPage = () => {
+
+	const router = useRouter();
+
+	useEffect(() => {
+		setTimeout(() => {
+			router.push('/');
+		}, 3000);
+	}, []);
+
 	return (
 		<div className='not-found'>
 			<h1>ОЙ...</h1>
 			<h2>Такой страницы нет!</h2>
-			<p>Перейти на <Link href='/'>главную страницу</Link></p>
+			<p>Перехожу  на <Link href='/'>главную страницу</Link> через 3 секунды...</p>
 		</div>
 	)
 }
